e34c5c55113342d5b9ae0e7142e1264e21d95876
Bruno Dias <brunodg.dev@gmail.com>
2022-09-23 22:33:28 -0300

    feat: Enemies following the waypoints as intended.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * Time.deltaTime * speed, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.waypoints[wavepointIndex];
+    }
+}
